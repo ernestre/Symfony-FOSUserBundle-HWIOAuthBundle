09589c6ac31db01c5d3f13d626d0815af400d6ab
Ernest Rekel <ernest.rekel@gmail.com>
2016-09-27 11:57:26 +0300

    Add base route tests

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -15,4 +15,44 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+    /**
+     * @dataProvider providerRouteProvider
+     */
+    public function testProviderRoutes($route, $content)
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $route);
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertContains($content, $crawler->filter('a')->text());
+    }
+
+    public function providerRouteProvider()
+    {
+        return [
+            ['/login/facebook','https://www.facebook.com/v2.0/dialog/oauth?'],
+            ['/login/google', 'https://accounts.google.com/o/oauth2/auth?'],
+        ];
+    }
+
+    /**
+     * @dataProvider baseRouteProvider
+     */
+    public function testBaseRoutes($route)
+    {
+        $client = static::createClient();
+        $client->request('GET', $route);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function baseRouteProvider()
+    {
+        return [
+            ['/login'],
+            ['/register/'],
+            ['/resetting/request'],
+        ];
+    }
 }
